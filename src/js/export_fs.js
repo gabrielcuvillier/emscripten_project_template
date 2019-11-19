0d87fe2ecba46ea54231946f6c559510af15e851
Gabriel <gabriel.cuvillier@continuation-labs.com>
2019-11-19 17:12:11 +0100

    tweaks

--- src/js/export_fs.js
+++ src/js/export_fs.js
@@ -1,29 +1,14 @@
 // Copyright (c) 2019 Gabriel Cuvillier, Continuation Labs (www.continuation-labs.com)
 // Licensed under CC0 1.0 Universal
 
-if (Module['postRun'] instanceof Array) {
-  Module['postRun'].push(export_fs);
+if (Module['preRun'] instanceof Array) {
+  Module['preRun'].push(export_idbfs);
 } else {
-  Module['postRun'] = [export_fs];
+  Module['preRun'] = [export_idbfs];
 }
 
-function export_fs() {
-  Module.FS_mkdir = function (a) {
-    return Module.FS.mkdir(a)
-  };
-  Module.FS_readFile = function (a) {
-    return Module.FS.readFile(a);
-  };
-  Module.FS_writeFile = function (a, b, c) {
-    return Module.FS.writeFile(a, b, c)
-  };
-  Module.FS_unlink = function (a) {
-    return Module.FS.unlink(a)
-  };
-  Module.FS_mount = function (a, b) {
-    return Module.FS.mount(a, b)
-  };
-  Module.FS_rmdir = function (a) {
-    return Module.FS.rmdir(a)
-  };
+function export_idbfs() {
+  Module['IDBFS'] = IDBFS;
+  Module['MEMFS'] = MEMFS;
+  Module['WORKERFS'] = WORKERFS;
 }
